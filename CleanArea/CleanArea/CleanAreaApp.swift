dba693c83d7be8182e0a96576d9b959077c52265
최동호 <choedongho@choedonghoui-MacBookPro.local>
2023-12-05 14:29:17 +0900

    Add: Project 생성

---
+++ CleanArea/CleanArea/CleanAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanAreaApp.swift
+//  CleanArea
+//
+//  Created by 최동호 on 12/5/23.
+//
+
+import SwiftUI
+
+@main
+struct CleanAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
