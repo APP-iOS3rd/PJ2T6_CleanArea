ae5308b9ee594c4926f4ee28427ec8978dc80554
NohJooYoung <nohjoo97@naver.com>
2023-12-06 11:11:10 +0900

    Initial commit

---
+++ CleanArea/CleanArea/iOS/Detail/DetailScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailScrollView.swift
+//  CleanArea
+//
+//  Created by 노주영 on 2023/12/06.
+//
+
+import SwiftUI
+
+struct DetailScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailScrollView()
+}
