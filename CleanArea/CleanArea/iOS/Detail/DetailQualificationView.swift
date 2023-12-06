bebc7da3c9a47bc37a3ee6b32f777b71b92c85bb
NohJooYoung <nohjoo97@naver.com>
2023-12-06 13:15:36 +0900

    Initial commit

---
+++ CleanArea/CleanArea/iOS/Detail/DetailQualificationView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailQualificationView.swift
+//  CleanArea
+//
+//  Created by 노주영 on 2023/12/06.
+//
+
+import SwiftUI
+
+struct DetailQualificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailQualificationView()
+}
---
+++ CleanArea/CleanArea/iOS/Detail/DetailWayView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailWayView.swift
+//  CleanArea
+//
+//  Created by 노주영 on 2023/12/06.
+//
+
+import SwiftUI
+
+struct DetailWayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailWayView()
+}
