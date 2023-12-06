761d03fe304cc1877c711a07fae0416988ef8164
NohJooYoung <nohjoo97@naver.com>
2023-12-06 16:14:04 +0900

    design: 신청방법 UI 구현

--- CleanArea/CleanArea/iOS/Detail/DetailWayView.swift
+++ CleanArea/CleanArea/iOS/Detail/DetailWayView.swift
@@ -8,11 +8,51 @@
 import SwiftUI
 
 struct DetailWayView: View {
+    @Binding var selectedIndex: Int
+    
+    var proxy: ScrollViewProxy?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Text("신청방법")
+                    .font(.system(size: 20, weight: .semibold))
+                    .foregroundStyle(Color.init(hex: "1E482D"))
+                Spacer()
+            }
+            .frame(width: UIScreen.main.bounds.width - 40)
+            .padding(.top, 30)
+            
+            Text("신청절차")
+                .modifier(DetailSemiTitleModifier())
+            Text("-")
+                .modifier(DetailSemiContentModifier())
+                .padding(.top, -10)
+            
+            Text("심시 및 발표")
+                .modifier(DetailSemiTitleModifier())
+            Text("-")
+                .modifier(DetailSemiContentModifier())
+                .padding(.top, -10)
+            
+            Text("신청 사이트")
+                .modifier(DetailSemiTitleModifier())
+            Text("-")
+                .modifier(DetailSemiContentModifier())
+                .padding(.top, -10)
+            
+            Text("제출 서류")
+                .modifier(DetailSemiTitleModifier())
+            Text("수영구청 청년포털 홈페이지 공고문 참조")
+                .modifier(DetailSemiContentModifier())
+                .padding(.top, -10)
+        }
+        .id(2)
+        .padding(.horizontal, 20)
     }
 }
 
-#Preview {
-    DetailWayView()
-}
+/*
+ #Preview {
+ DetailWayView()
+ }*/
