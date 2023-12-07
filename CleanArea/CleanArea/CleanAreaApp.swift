78c4ecee36247ab9fc6ee87cb5329aa520c6f202
NohJooYoung <nohjoo97@naver.com>
2023-12-07 11:15:28 +0900

    Initial commit

---
+++ CleanArea/CleanArea/CleanAreaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CleanAreaApp.swift
+//  CleanArea
+//
+//  Created by 노주영 on 2023/12/07.
+//
+
+import SwiftUI
+
+@main
+struct CleanAreaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CleanArea/CleanArea/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CleanArea
+//
+//  Created by 노주영 on 2023/12/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
