fbf985f8872095c7ec38cba557686baac9910397
NohJooYoung <nohjoo97@naver.com>
2023-12-11 16:23:40 +0900

    Initial commit

---
+++ CleanArea/CleanArea/iOS/WebSite/WebSiteLinkView.swift
@@ -0,0 +1,18 @@
+//
+//  WebSiteLinkView.swift
+//  CleanArea
+//
+//  Created by 노주영 on 12/11/23.
+//
+
+import SwiftUI
+
+struct WebSiteLinkView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebSiteLinkView()
+}
---
+++ CleanArea/CleanArea/iOS/WebSite/WebSiteView.swift
@@ -0,0 +1,18 @@
+//
+//  WebSiteView.swift
+//  CleanArea
+//
+//  Created by 노주영 on 12/11/23.
+//
+
+import SwiftUI
+
+struct WebSiteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebSiteView()
+}
