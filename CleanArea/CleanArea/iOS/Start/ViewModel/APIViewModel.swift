26a9865fc0ddd2ab02606194e6e0e14254c28851
funMango <138420832+funMango@users.noreply.github.com>
2023-12-12 17:35:56 +0900

    refactor: API 전체 데이터 호출

--- CleanArea/CleanArea/iOS/Start/ViewModel/APIViewModel.swift
+++ CleanArea/CleanArea/iOS/Start/ViewModel/APIViewModel.swift
@@ -36,21 +36,41 @@ class APIViewModel: XMLParser, ObservableObject {
     //query: 정책명,정책소개 정보검색, bizTycdSel: 정책분야, srchPolyBizSecd: 주거지 ,keyword: 키워드
     func search(vm: StartVM) {
         guard let apiKey = apiKey else { return }
-        guard let url = URL(string: getURL(apiKey, vm.policyName, vm.residence?.rawValue ?? "")) else { return }
         
-        fetchAndParseXML(from: url) { youthPolicyList in
-            if let youthPolicyList = youthPolicyList {
-                DispatchQueue.main.async {
-                    
-                    let filtered = self.filter(youthPolicyList.youthPolicies, vm)
-                    print(filtered.count)
-                    self.result = youthPolicyList.youthPolicies
-                    let policy = Policy()
-                    policy.getPolicy(filtered)
-                    self.policy = policy                
+        DispatchQueue.global().async {
+            let dispatchGroup = DispatchGroup()
+            var result: [YouthPolicy] = []
+            let policy = Policy()
+
+            for index in 1...7 {
+                dispatchGroup.enter()
+
+                guard let url = URL(string: self.getURL(apiKey, vm.policyName, index)) else {
+                    dispatchGroup.leave()
+                    return
+                }
+
+                self.fetchAndParseXML(from: url) { youthPolicyList in
+                    defer {
+                        dispatchGroup.leave()
+                    }
+
+                    if let youthPolicyList = youthPolicyList {
+                        let filtered = self.filter(youthPolicyList.youthPolicies, vm)
+                        print(filtered.count)
+                        result += youthPolicyList.youthPolicies
+                        policy.getPolicy(filtered)
+                    }
                 }
             }
+
+            dispatchGroup.notify(queue: .main) {
+                // 여기서는 모든 백그라운드 작업이 완료된 후에 실행할 코드를 작성합니다.
+                self.policy = policy
+                self.result = result
+            }
         }
+
     }
 }
 
@@ -70,11 +90,11 @@ extension APIViewModel {
         task.resume()
     }
     
-    func getURL(_ apiKey: String, _ query: String, _ srchPolyBizSecd: String) -> String {
+    func getURL(_ apiKey: String, _ query: String, _ index: Int) -> String {
         let BASE_URL = "https://www.youthcenter.go.kr/opi/youthPlcyList.do"
         
         if query.isEmpty  {
-            return "\(BASE_URL)?openApiVlak=\(apiKey)&display=100&pageIndex=1&srchPolyBizSecd=003002008,\(srchPolyBizSecd)"
+            return "\(BASE_URL)?openApiVlak=\(apiKey)&display=100&pageIndex=\(index)&srchPolyBizSecd=003002008"
         } else {
             return "\(BASE_URL)?openApiVlak=\(apiKey)&display=100&pageIndex=1&query=\(query)"
         }
